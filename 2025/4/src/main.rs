71a3b08afca46001c8b6aa619f9eabbc67f8c064
Noah765 <noah.landgraf@gmx.de>
2026-07-04 19:31:41 +0200

    rust: remove rustfmt.toml

--- 2025/4/src/main.rs
+++ 2025/4/src/main.rs
@@ -1,54 +1,75 @@
 fn main() {
-  advent_of_code::run(first, second);
+    advent_of_code::run(first, second);
 }
 
 fn first(input: String) -> usize {
-  get_accessible_paper_positions(&construct_grid(&input)).len()
+    get_accessible_paper_positions(&construct_grid(&input)).len()
 }
 
 fn second(input: String) -> usize {
-  let mut grid = construct_grid(&input);
+    let mut grid = construct_grid(&input);
 
-  let mut result = 0;
+    let mut result = 0;
 
-  let mut accessible_paper_positions = get_accessible_paper_positions(&grid);
-  while !accessible_paper_positions.is_empty() {
-    result += accessible_paper_positions.len();
+    let mut accessible_paper_positions = get_accessible_paper_positions(&grid);
+    while !accessible_paper_positions.is_empty() {
+        result += accessible_paper_positions.len();
 
-    for (x, y) in accessible_paper_positions {
-      grid[y][x] = Cell::Empty;
+        for (x, y) in accessible_paper_positions {
+            grid[y][x] = Cell::Empty;
+        }
+
+        accessible_paper_positions = get_accessible_paper_positions(&grid);
     }
 
-    accessible_paper_positions = get_accessible_paper_positions(&grid);
-  }
-
-  result
+    result
 }
 
 #[derive(PartialEq)]
 enum Cell {
-  Empty,
-  Paper,
+    Empty,
+    Paper,
 }
 
 fn construct_grid(input: &str) -> Vec<Vec<Cell>> {
-  input.lines().map(|x| x.chars().map(|x| if x == '@' { Cell::Paper } else { Cell::Empty }).collect()).collect()
+    input
+        .lines()
+        .map(|x| {
+            x.chars()
+                .map(|x| if x == '@' { Cell::Paper } else { Cell::Empty })
+                .collect()
+        })
+        .collect()
 }
 
 fn get_accessible_paper_positions(grid: &Vec<Vec<Cell>>) -> Vec<(usize, usize)> {
-  grid.iter().enumerate().map(|(y, row)| row.iter().enumerate().filter(move |(x, cell)| **cell == Cell::Paper && count_surrounding_papers(&grid, *x, y) < 4).map(move |(x, _)| (x, y))).flatten().collect()
+    grid.iter()
+        .enumerate()
+        .map(|(y, row)| {
+            row.iter()
+                .enumerate()
+                .filter(move |(x, cell)| {
+                    **cell == Cell::Paper && count_surrounding_papers(&grid, *x, y) < 4
+                })
+                .map(move |(x, _)| (x, y))
+        })
+        .flatten()
+        .collect()
 }
 
 fn count_surrounding_papers(grid: &Vec<Vec<Cell>>, x: usize, y: usize) -> usize {
-  let surrounding = [
-    grid.get(y - 1).and_then(|row| row.get(x - 1)),
-    grid.get(y - 1).map(|row| &row[x]),
-    grid.get(y - 1).and_then(|row| row.get(x + 1)),
-    grid[y].get(x + 1),
-    grid[y].get(x - 1),
-    grid.get(y + 1).and_then(|row| row.get(x - 1)),
-    grid.get(y + 1).map(|row| &row[x]),
-    grid.get(y + 1).and_then(|row| row.get(x + 1)),
-  ];
-  surrounding.iter().filter(|x| x.is_some_and(|x| *x == Cell::Paper)).count()
+    let surrounding = [
+        grid.get(y - 1).and_then(|row| row.get(x - 1)),
+        grid.get(y - 1).map(|row| &row[x]),
+        grid.get(y - 1).and_then(|row| row.get(x + 1)),
+        grid[y].get(x + 1),
+        grid[y].get(x - 1),
+        grid.get(y + 1).and_then(|row| row.get(x - 1)),
+        grid.get(y + 1).map(|row| &row[x]),
+        grid.get(y + 1).and_then(|row| row.get(x + 1)),
+    ];
+    surrounding
+        .iter()
+        .filter(|x| x.is_some_and(|x| *x == Cell::Paper))
+        .count()
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,37 +1,43 @@
 use std::{
-  env, fs,
-  io::{self, Read},
-  time::Instant,
+    env, fs,
+    io::{self, Read},
+    time::Instant,
 };
 
 use serde::Serialize;
 
 pub fn run<F, T, S, U>(first: F, second: S)
 where
-  F: FnOnce(String) -> T,
-  T: Serialize,
-  S: FnOnce(String) -> U,
-  U: Serialize,
+    F: FnOnce(String) -> T,
+    T: Serialize,
+    S: FnOnce(String) -> U,
+    U: Serialize,
 {
-  let mut input = String::new();
-  io::stdin().read_to_string(&mut input).expect("stdin should contain valid UTF8");
-  let args: Vec<_> = env::args().collect();
+    let mut input = String::new();
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("stdin should contain valid UTF8");
+    let args: Vec<_> = env::args().collect();
 
-  let start = Instant::now();
+    let start = Instant::now();
 
-  match &args[1][..] {
-    "first" => {
-      let result = first(input);
-      write_result(&args[2], (start.elapsed(), result));
-    }
-    "second" => {
-      let result = second(input);
-      write_result(&args[2], (start.elapsed(), result));
-    }
-    _ => panic!(r#"the first arg should be either "first" or "second""#),
-  };
+    match &args[1][..] {
+        "first" => {
+            let result = first(input);
+            write_result(&args[2], (start.elapsed(), result));
+        }
+        "second" => {
+            let result = second(input);
+            write_result(&args[2], (start.elapsed(), result));
+        }
+        _ => panic!(r#"the first arg should be either "first" or "second""#),
+    };
 }
 
 fn write_result(file: &str, value: impl Serialize) {
-  fs::write(file, serde_json::to_string(&value).expect("should be able to serialize output")).unwrap_or_else(|_| panic!("should be able to write to {file}"));
+    fs::write(
+        file,
+        serde_json::to_string(&value).expect("should be able to serialize output"),
+    )
+    .unwrap_or_else(|_| panic!("should be able to write to {file}"));
 }
