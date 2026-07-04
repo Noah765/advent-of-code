c27dff3786d1579d9c1ffa55d619c5019b74591e
Noah765 <noah.landgraf@gmx.de>
2026-07-04 19:31:41 +0200

    flake: update

--- flake.nix
+++ flake.nix
@@ -1,24 +1,45 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    treefmt.url = "github:numtide/treefmt-nix";
+    treefmt.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: let
-    inherit (nixpkgs.lib) genAttrs getExe makeBinPath mapAttrs systems;
-    forAllSystems = f: mapAttrs f (genAttrs systems.flakeExposed (x: nixpkgs.legacyPackages.${x}));
+  outputs = inputs: let
+    inherit (inputs.nixpkgs.lib) genAttrs getExe makeBinPath systems;
+    eachSystem = f: genAttrs systems.flakeExposed (x: f inputs.nixpkgs.legacyPackages.${x});
+
+    formatter = pkgs:
+      (inputs.treefmt.lib.evalModule pkgs {
+        programs.alejandra.enable = true;
+        programs.rustfmt.enable = true;
+      }).config.build.wrapper;
   in {
-    devShells = forAllSystems (system: pkgs: {default = pkgs.mkShell {packages = [pkgs.alejandra pkgs.rustfmt self.packages.${system}.aoc];};});
-    packages = forAllSystems (_: pkgs: rec {
+    packages = eachSystem (pkgs: rec {
+      default = aoc;
+
       aoc =
         pkgs.writers.writeNuBin "aoc" {
           check = ''${getExe pkgs.nushell} --commands "nu-check --debug $out"'';
           makeWrapperArgs = ["--prefix PATH : ${makeBinPath [pkgs.cargo pkgs.nushell]}"];
         }
         ./aoc.nu;
-      default = aoc;
     });
-    formatter = forAllSystems (_: pkgs: pkgs.alejandra);
+
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          cargo
+          clippy
+          nushell
+          rust-analyzer
+          rustc
+          inputs.self.packages.${pkgs.stdenv.system}.aoc
+          (formatter pkgs)
+        ];
+      };
+    });
+
+    formatter = eachSystem formatter;
   };
 }
